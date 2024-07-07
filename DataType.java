3064dbf87b8d716784dfc5e9b9401e7c6f481c4a
INDHUPRIYA-D-80 <indhumirthi08@gmail.com>
2024-07-07 22:57:47 +0530

    Day-2

---
+++ DataType.java
@@ -0,0 +1,26 @@
+public class DataType {
+    public static void main(String[] args) {
+        int myNum1 = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;     // Floating point number
+        char myLetter = 'D';          // Character
+        boolean myBool = true;        // Boolean
+        String myText = "Hello";      // String
+
+        int myNum2 = 100000;
+        System.out.println(myNum2);
+
+        long myLongNum = 15000000000L;
+        System.out.println(myLongNum);
+
+        float myFloatNum2 = 5.75f;
+        System.out.println(myFloatNum2);
+
+        double myDoubleNum = 19.99d;
+        System.out.println(myDoubleNum);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+    }
+}
