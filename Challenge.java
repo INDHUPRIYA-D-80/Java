57ca220d878e7c84f1c0c70fde6837e1ab8b0e19
INDHUPRIYA-D-80 <indhumirthi08@gmail.com>
2024-07-09 22:50:43 +0530

    Day-4

---
+++ Challenge.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Challenge {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    // String name =scan.nextLine();
+    // int age = scan.nextInt();
+    // scan.nextLine();
+    // String address = scan.nextLine();
+    // scan.nextLine();
+    // System.out.println("Your name is "+name);
+    // System.out.println("Your Age is "+age);
+    // System.out.println("Your Address is "+address);
+    // int a = scan.nextInt();
+    // int b = scan.nextInt();
+    // int c = scan.nextInt();
+    // int d=a*b*c;
+    // int e=a+b+c;
+    // System.out.println(d/e);
+    String dept =scan.nextLine();
+    int m1 = scan.nextInt();
+    int m2 = scan.nextInt();
+    int m3 = scan.nextInt();
+    double m=m1+m2+m3;
+    System.out.println(m/10 +"/10");
+    System.out.println("Your department is "+dept);
+    
+    }
+    
+}
